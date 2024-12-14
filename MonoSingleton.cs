3b70cb6ef91c010d79228a0f05728df7c4e40a3e
cangmingcc <3325485079@qq.com>
2024-12-14 08:56:07 +0800

    继承MonoBehaviour的单例

---
+++ MonoSingleton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance==null)
+            {
+                Debug.LogError("can not find MonoSingleton");
+            }
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        instance = this as T;
+    }
+}
